1d8684805a6955439233e3aaca5b0bf6412f3d05
Vitas2048 <vithag97@mail.ru>
2022-02-21 22:05:10 +0700

    1.2. Входные данные в тесте [#297750]

--- src/test/java/ru/job4j/MainTest.java
+++ src/test/java/ru/job4j/MainTest.java
@@ -5,6 +5,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class MainTest {
+
     @Test
     public void whenActionThenResult() {
         int excepted = 1;
