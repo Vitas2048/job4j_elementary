9dadf22ef4162f8fdeda0e03f70832216ce1a6ee
Vitas2048 <vithag97@mail.ru>
2022-02-27 17:13:38 +0700

    5.4. Протеиновая диета [#156313]

--- src/main/java/ru/job4j/loop/Fitness.java
+++ src/main/java/ru/job4j/loop/Fitness.java
@@ -3,12 +3,9 @@ package ru.job4j.loop;
 public class Fitness {
     public static int calc(int ivan, int nik) {
         int month = 0;
-        while (ivan < nik) {
+        while (ivan <= nik) {
             ivan *= 3;
             nik *= 2;
-            month++;
-        }
-        if (ivan == nik) {
             month++;
         }
         return month;
