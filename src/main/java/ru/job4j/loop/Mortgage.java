ea2f329f865dd1d5fc68703c682ad79be37efa8c
Vitas2048 <vithag97@mail.ru>
2022-02-28 23:27:54 +0700

    5.7. Ипотека [#156315]

---
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,20 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public static int year(double amount, int salary, double percent) {
+        int year = 0;
+
+        while ((amount + (amount * percent / 100)) >= salary) {
+            amount = amount + (amount * percent / 100) - salary;
+            year++;
+        }
+            if ((amount > 0) && (amount < salary)) {
+                year++;
+            } else {
+                if ((amount + (amount * percent / 100)) <= salary) {
+                    year = 1;
+                }
+            }
+        return year;
+    }
+}
