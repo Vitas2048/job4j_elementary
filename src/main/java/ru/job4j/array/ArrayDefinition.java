c431b429707cd11aedc3e765eddf93fbc4b5cbef
Vitas2048 <vithag97@mail.ru>
2022-03-01 23:32:15 +0700

    6.0.2. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,8 +5,14 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
-        System.out.println("размер ages - " + ages.length);
-        System.out.println("размер surnames - " + surnames.length);
-        System.out.println("размер prices - " + prices.length);
-    }
+        String[] names = new String[5];
+        names[0] = "Петр Арсентьев";
+        names[1] = "Коробейников Стас";
+        names[2] = "Карташова Елена";
+        names[3] = "Хагай Виталий";
+        names[4] = "Фамилия Имя";
+        for (int i = 0; i < 5; i++) {
+            System.out.println(names[i]);
+        }
+     }
 }
